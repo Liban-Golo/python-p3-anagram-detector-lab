4371a943555f7df0eae2a2a37b0f18af896054ca
Liban Golo <liban.golo@student.moringaschool.com>
2025-11-28 07:23:54 +0300

    anagram detector done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        matches = []
+        sorted_original = sorted(self.word)
+
+        for w in word_list:
+        
+            if w.lower() != self.word:
+                if sorted(w.lower()) == sorted_original:
+                    matches.append(w)
+
+        return matches
